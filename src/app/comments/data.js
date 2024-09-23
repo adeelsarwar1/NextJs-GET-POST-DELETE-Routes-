209b7774efa7b7697e31daf68669c39422af57c3
adeelsarwar1 <adeel2694551@gmail.com>
2024-09-23 23:38:27 +0500

    GET & POST Request

---
+++ src/app/comments/data.js
@@ -0,0 +1,13 @@
+export const comments = [
+    {
+        id: 1,
+        text: "This is a comment",
+    },
+    {
+        id: 2,
+        text: "This is a comment",
+    },{
+        id: 3,
+        text: "This is a comment",
+    },
+];
